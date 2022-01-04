0f9e41f9cdb650e6794b5eb61ab05fbcc87b0283
Nick <nsagias@gmail.com>
2022-01-04 18:00:25 -0500

    add content to EfexampleContext

--- SqlServerEFExample/EFExampleContext.cs
+++ SqlServerEFExample/EFExampleContext.cs
@@ -4,6 +4,15 @@ using Microsoft.EntityFrameworkCore;
 namespace SqlServerEFExample {
 
   public class EFExampleContext : DbContext {
-    
+    string _connectionString;
+    public EFExampleContext(string connectionString) {
+      this._connectionString = connectionString;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlServer(this._connectionString);
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Task> Tasks { get; set; }
   }
 }
