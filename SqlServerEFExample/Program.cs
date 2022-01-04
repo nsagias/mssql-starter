31ee0cf7fe9b40cc8ab3d91bfe11d7140def42cd
Nick <nsagias@gmail.com>
2022-01-04 18:06:30 -0500

    Add connection string and try catch

--- SqlServerEFExample/Program.cs
+++ SqlServerEFExample/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace SqlServerEFExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Program {
+      static void Main(string[] args){
+          
+        try {
+          // connection string
+          SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+          builder.DataSource = "localhost";
+          builder.UserID = "sa";
+          builder.Password = "SuperSecret1";
+          builder.InitialCatalog = "master";
+          
+        } catch (Exception e) {
+          Console.WriteLine(e.ToString());
         }
+
+        Console.WriteLine("All done. Press any key to finish...,");
+        Console.ReadKey(true);
+        
+      }
     }
 }
