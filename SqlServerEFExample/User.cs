140f2e513255b52a513f8533cda7919afb8983d4
Nick <nsagias@gmail.com>
2022-01-04 17:35:36 -0500

    Create user model/class

---
+++ SqlServerEFExample/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlServerEFExample
+{
+    class User
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
