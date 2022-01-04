bd0c3d2f3f15131103c55e25dd9d0d2e14098fc5
Nick <nsagias@gmail.com>
2022-01-03 19:41:14 -0500

    Add connection to database

--- SqlServerSample/Program.cs
+++ SqlServerSample/Program.cs
@@ -8,11 +8,22 @@ namespace SqlServerSample
     {
         static void Main(string[] args)
         {
+            // connection string
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "localhost";
             builder.UserID = "sa";
             builder.Password = "secret";
             builder.InitialCatalog = "master";
+
+            // connect to mssql server
+            Console.WriteLine("Conecting to MSSQL ....");
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString)){
+              connection.Open();
+              Console.WriteLine("Connected.");
+            }
+
+            Console.WriteLine("All done.  Press any key to finish ...");
+            Console.ReadKey(true);
         }
     }
 }
