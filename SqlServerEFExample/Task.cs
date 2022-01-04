c4f8c8ce7d2ae40f6ef86018861234e700b2ee35
Nick <nsagias@gmail.com>
2022-01-04 17:51:22 -0500

    Create model/class/file Task.cs

--- SqlServerEFExample/Task.cs
+++ SqlServerEFExample/Task.cs
@@ -3,7 +3,16 @@ using System;
 namespace SqlServerEFExample {
   
   public class Task {
-    
+    public int TaskId { get; set; }
+    public string Title { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsComplete { get; set; }
+    public virtual User AssignedTo { get; set; }
+
+    public override string ToString()
+    {
+      return @$"Task [ Id={this.TaskId}, Title={this.Title}, Due-Date={this.DueDate.ToString()}, IsComplete={this.IsComplete} ]";
+    }
   }
 
 }
