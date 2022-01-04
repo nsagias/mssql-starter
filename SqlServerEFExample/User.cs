82b5748ae134978fe79048632f1e3b42a6e40e5d
Nick <nsagias@gmail.com>
2022-01-04 17:43:45 -0500

    To string all the values

--- SqlServerEFExample/User.cs
+++ SqlServerEFExample/User.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 
 namespace SqlServerEFExample
 {
-    class User
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+    public class User {
+      public int UserId { get; set; }
+      public String FirstName { get; set; }
+      public String LastName { get; set; }
+      public virtual IList<Task> Task { get; set; }
+
+      // return full name string
+      public String GetFullName() {
+        return $"{this.FirstName} {this.LastName}";
+      }
+      // return var in a string
+      public override string ToString() {
+        return $"User [ id={this.UserId}, name={this.GetFullName()} ]";
+      }
+
     }
 }
